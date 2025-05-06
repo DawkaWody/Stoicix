c14002d274f36c21507fefeabad735968ddaf1a0
troublemake3r <m.a.dukaczewska@gmail.com>
2025-05-06 17:01:15 +0200

    dodano inny ysort

--- Stoicix/Assets/Scripts/Objects/SortingOrderHandling.cs
+++ Stoicix/Assets/Scripts/Objects/SortingOrderHandling.cs
@@ -1,8 +1,8 @@
 using UnityEngine;
 
-public class ColumnDeco : MonoBehaviour
+public class SortingOrderHandling : MonoBehaviour
 {
-    [SerializeField] private SpriteRenderer[] _sprites;
+    /*[SerializeField] private SpriteRenderer[] _sprites;
     [SerializeField] private float _yOffset = 0.5f;
 
     private Vector2 _checkPosition;
@@ -18,7 +18,7 @@ public class ColumnDeco : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
         if (_player == null) return;
 
@@ -30,5 +30,21 @@ public class ColumnDeco : MonoBehaviour
         {
             foreach (SpriteRenderer colPart in _sprites) colPart.sortingOrder = _playerSortingOrder - 1;
         }
+    }*/
+    [SerializeField] private SpriteRenderer[] _sprites;
+    [SerializeField] private int sortingOffset = 1000; // to round to int
+    [SerializeField] private float referenceYOffset = 0f;
+
+    public int sortord;
+    void LateUpdate()
+    {
+        if (_sprites == null || _sprites.Length == 0) return;
+
+        float baseY = transform.position.y + referenceYOffset;
+        int sortOrder = Mathf.RoundToInt(-baseY * sortingOffset);
+        //sortOrder /= sortingOffset;
+
+        foreach (var sprite in _sprites) sprite.sortingOrder = sortOrder;
+        sortord = sortOrder;
     }
 }
