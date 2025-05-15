cfb372c44bc80a94cc00c17e8f7aa3e4466c9f58
DawkaWody <dawkawody@wp.pl>
2025-05-15 20:38:24 +0200

    Dokończono minigame 1

--- Stoicix/Assets/Scripts/Minigames/Minigame1/Bar.cs
+++ Stoicix/Assets/Scripts/Minigames/Minigame1/Bar.cs
@@ -1,118 +1,21 @@
-using System.Collections;
+using System;
 using UnityEngine;
 
-[RequireComponent(typeof(Rigidbody2D))]
-public class Bar : MonoBehaviour
+[RequireComponent(typeof(BarLift))]
+public class Bar : MonoBehaviour, IInteractable
 {
-    [SerializeField] private float _targetCps;
-    [SerializeField] private float _duraton;
-    [SerializeField] private float _minY;
-    [SerializeField] private float _maxY;
-    [SerializeField] private float _forceMultiplierBecauseChatgptSucks = 1f;
+    public int InteractPriority { get; set; }
 
-    private float _startY;
-    private float _clickForce;
-    private float _gameTimer;
-    private bool _gameActive;
-    private bool _gameStarted;
-    private bool _skipNextGravityCheck;
+    private BarLift _barLift;
 
-    private int _clickCountThisSecond;
-    private float _cpsTimer;
-
-    private Rigidbody2D _rigidbody;
-    private PlayerInputHandler _playerInputHandler;
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        _rigidbody = GetComponent<Rigidbody2D>();
-        _playerInputHandler = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerInputHandler>();
-
-        _startY = transform.position.y;
-        _clickForce = _rigidbody.mass * (_maxY - _minY) / _targetCps;
-        _clickForce *= _forceMultiplierBecauseChatgptSucks; // force to apply to the object when clicked
-        _gameTimer = _duraton;
-        _rigidbody.gravityScale = 0f;
-        _gameActive = true;
-        _gameStarted = false;
-        _skipNextGravityCheck = false;
+        _barLift = GetComponent<BarLift>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Interact(IInteractable.EmptyCallback onInteractionEnd = null)
     {
-        if (transform.position.y > _maxY)
-        {
-            Debug.Log("Clamping Y");
-            transform.position = new Vector3(transform.position.x, _maxY, transform.position.z);
-            _rigidbody.linearVelocity = Vector2.zero;
-        }
-
-        if (!_gameActive) return;
-
-        //cps for fun
-        _cpsTimer += Time.deltaTime;
-
-        if (_playerInputHandler.MouseWasPressed)
-        {
-            _clickCountThisSecond++;
-
-            _rigidbody.gravityScale = 1f;
-            _skipNextGravityCheck = true; // skip the next gravity check
-            Debug.Log(_rigidbody.gravityScale);
-            _rigidbody.AddForce(Vector2.up * _clickForce, ForceMode2D.Impulse);
-        }
-
-        // Log CPS every second
-        if (_cpsTimer >= 1f)
-        {
-            Debug.Log($"CPS: {_clickCountThisSecond}");
-            _clickCountThisSecond = 0;
-            _cpsTimer = 0f;
-        }
-
-        StartCoroutine(GravityResetCheck());
-
-        if (!_gameStarted)
-        {
-            if (transform.position.y > _minY)
-            {
-                _gameStarted = true;
-            }
-            else return;
-        }
-
-        if (transform.position.y < _minY)
-        {
-            _gameActive = false;
-            Debug.Log("Loser hahahhahahhahahahahahahahahahhahahahahahhahahahhahahahah");
-        }
-        else
-        {
-            _gameTimer -= Time.deltaTime;
-            if (!(_gameTimer <= 0)) return;
-            _gameActive = false;
-            Debug.Log("Winner yayayayayayayayayayayayayayyayyyayayay");
-        }
-
-        // Disable gravity after the game ends
-        _rigidbody.gravityScale = 0f;
-        _rigidbody.linearVelocity = Vector2.zero;
-    }
-
-    private IEnumerator GravityResetCheck()
-    {
-        if (_skipNextGravityCheck)
-        {
-            yield return new WaitForEndOfFrame();
-            _skipNextGravityCheck = false;
-        }
-        yield return new WaitForEndOfFrame();
-        if (!(transform.position.y <= _startY)) yield break;
-        Debug.Log("Gravity reset because _startY reached");
-        transform.position = new Vector3(transform.position.x, _startY, transform.position.z);
-        _rigidbody.linearVelocity = Vector2.zero;
-        _rigidbody.gravityScale = 0f;
+        Debug.Log("skibidey game start");
+        _barLift.StartGame(onInteractionEnd);
     }
 }
---
+++ Stoicix/Assets/Scripts/Minigames/Minigame1/BarLift.cs
@@ -0,0 +1,145 @@
+using System.Collections;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class BarLift : MonoBehaviour
+{
+    [SerializeField] private float _targetCps;
+    [SerializeField] private float _duraton;
+    [SerializeField] private float _minY;
+    [SerializeField] private float _maxY;
+    [SerializeField] private float _forceMultiplierBecauseChatgptSucks = 1f;
+    [SerializeField] private float _endDelay = 0.3f;
+
+    private float _startY;
+    private float _clickForce;
+    private float _gameTimer;
+    private bool _gameActive;
+    private bool _gameStarted;
+    private bool _skipNextGravityCheck;
+    private bool _gameWon;
+    private int _clickCountThisSecond;
+    private float _cpsTimer;
+    private IInteractable.EmptyCallback _onGameEnd;
+
+    private Rigidbody2D _rigidbody;
+    private PlayerInputHandler _playerInputHandler;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+        _playerInputHandler = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerInputHandler>();
+
+        _startY = transform.position.y;
+        _clickForce = _rigidbody.mass * (_maxY - _minY) / _targetCps;
+        _clickForce *= _forceMultiplierBecauseChatgptSucks; // force to apply to the object when clicked
+        _gameTimer = _duraton;
+        _rigidbody.gravityScale = 0f;
+        _gameActive = false;
+        _gameStarted = false;
+        _skipNextGravityCheck = false;
+        _gameWon = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!_gameActive) StartCoroutine(GravityResetCheck());
+
+        if (transform.position.y > _maxY)
+        {
+            transform.position = new Vector3(transform.position.x, _maxY, transform.position.z);
+            _rigidbody.linearVelocity = Vector2.zero;
+        }
+
+        if (!_gameActive) return;
+
+        //cps for fun
+        _cpsTimer += Time.deltaTime;
+
+        if (_playerInputHandler.MouseWasPressed)
+        {
+            _clickCountThisSecond++;
+
+            _rigidbody.gravityScale = 1f;
+            _skipNextGravityCheck = true; // skip the next gravity check
+            _rigidbody.AddForce(Vector2.up * _clickForce, ForceMode2D.Impulse);
+        }
+
+        // Log CPS every second
+        if (_cpsTimer >= 1f)
+        {
+            Debug.Log($"CPS: {_clickCountThisSecond}");
+            _clickCountThisSecond = 0;
+            _cpsTimer = 0f;
+        }
+
+        StartCoroutine(GravityResetCheck());
+
+        if (!_gameStarted)
+        {
+            if (transform.position.y > _minY)
+            {
+                _gameStarted = true;
+            }
+            else return;
+        }
+
+        if (transform.position.y < _minY)
+        {
+            _gameActive = false;
+            _gameWon = false;
+            Debug.Log("Loser hahahhahahhahahahahahahahahahhahahahahahhahahahhahahahah");
+        }
+        else
+        {
+            _gameTimer -= Time.deltaTime;
+            if (!(_gameTimer <= 0)) return;
+            _gameActive = false;
+            _gameWon = true;
+            Debug.Log("Winner yayayayayayayayayayayayayayyayyyayayay");
+        }
+
+        StartCoroutine(GameEndWithDelay(_gameWon));
+    }
+
+    private IEnumerator GravityResetCheck()
+    {
+        if (_skipNextGravityCheck)
+        {
+            yield return new WaitForEndOfFrame();
+            _skipNextGravityCheck = false;
+        }
+        yield return new WaitForEndOfFrame();
+        if (!(transform.position.y <= _startY)) yield break;
+        transform.position = new Vector3(transform.position.x, _startY, transform.position.z);
+        _rigidbody.linearVelocity = Vector2.zero;
+        _rigidbody.gravityScale = 0f;
+    }
+
+    private IEnumerator GameEndWithDelay(bool gameWon)
+    {
+        yield return new WaitForSeconds(_endDelay);
+        _onGameEnd?.Invoke();
+        if (!gameWon)
+        {
+            _gameTimer = _duraton;
+            _rigidbody.gravityScale = 0f;
+            _gameActive = false;
+            _gameStarted = false;
+            _skipNextGravityCheck = false;
+            _gameWon = false;
+        }
+        else
+        {
+            GameManager.Instance.LoadMainLevel();
+        }
+    }
+
+    public void StartGame(IInteractable.EmptyCallback onGameEnd)
+    {
+        _gameActive = true;
+        _onGameEnd = onGameEnd;
+    }
+}
