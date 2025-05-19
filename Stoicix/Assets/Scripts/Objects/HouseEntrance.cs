1f2c99a3f5cbc43c155077b78c4b3a7271bfbf6e
troublemake3r <m.a.dukaczewska@gmail.com>
2025-05-19 20:03:35 +0200

    lepsze wchodzenie do domu

--- Stoicix/Assets/Scripts/Objects/HouseEntrance.cs
+++ Stoicix/Assets/Scripts/Objects/HouseEntrance.cs
@@ -1,15 +1,37 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class HouseEntrance : MonoBehaviour
 {
     [SerializeField] private string houseSceneName = "Minigame1";
     [SerializeField] private Transform returnPositionOutside;
 
+    private PlayerInputHandler _playerInput;
+    private bool _playerInTrigger = false;
+
+    void Update()
+    {
+        if (_playerInTrigger && _playerInput != null && _playerInput.InteractWasPressed)
+        {
+            GameManager.Instance.EnterHouse(houseSceneName, returnPositionOutside.position);
+        }
+    }
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
-            GameManager.Instance.EnterHouse(houseSceneName, returnPositionOutside.position);
+            _playerInTrigger = true;
+            _playerInput = other.GetComponent<PlayerInputHandler>();
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            _playerInTrigger = false;
+            _playerInput = null;
         }
     }
 }
--- Stoicix/Assets/Scripts/Objects/HouseExit.cs
+++ Stoicix/Assets/Scripts/Objects/HouseExit.cs
@@ -1,14 +1,36 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class HouseExit : MonoBehaviour
 {
     [SerializeField] private string mainSceneName = "MainLevel";
 
+    private PlayerInputHandler _playerInput;
+    private bool _playerInTrigger = false;
+
+    void Update()
+    {
+        if (_playerInTrigger && _playerInput != null && _playerInput.InteractWasPressed)
+        {
+            GameManager.Instance.ExitHouse(mainSceneName);
+        }
+    }
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
-            GameManager.Instance.ExitHouse(mainSceneName);
+            _playerInTrigger = true;
+            _playerInput = other.GetComponent<PlayerInputHandler>();
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            _playerInTrigger = false;
+            _playerInput = null;
         }
     }
 }
